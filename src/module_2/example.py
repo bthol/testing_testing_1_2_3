f22be444e79b3069601c553ae343622f8fba9ead
bthol <bthollaug@gmail.com>
2026-04-06 20:14:05 -0700

    built

--- src/module_2/example.py
+++ src/module_2/example.py
@@ -1,5 +1,10 @@
-def subtract_one(a):
+import numpy as np
+
+def sub_pi(a):
     if isinstance(a, int) or isinstance(a, float):
-        return a - 1
+        return a - np.pi
     else:
         return False
+
+def cl_f():
+    return print('logged to the console from module_2')
